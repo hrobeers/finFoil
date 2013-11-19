c14b59014c1fcf70eb4f79438e7ca34903b2af86
hrobeers <hansrobeers@gmail.com>
2013-11-19 08:43:34 +0100

    Basic Length implementation

--- src/hrlib/hrlib.pri
+++ src/hrlib/hrlib.pri
@@ -5,11 +5,15 @@ HEADERS += \
     ../hrlib/math/brent.hpp \
     ../hrlib/qgraphics/pathutils.h \
     ../hrlib/hrlibfwd/qtfwd.h \
-    ../hrlib/math/spline.hpp
+    ../hrlib/math/spline.hpp \
+    ../hrlib/units/length.h \
+    ../hrlib/units/unitbase.h \
+    ../hrlib/units/iunit.h
 
 SOURCES += \
     ../hrlib/exceptions.cpp \
     ../hrlib/version.cpp \
     ../hrlib/math/brent.cpp \
     ../hrlib/qgraphics/pathutils.cpp \
-    ../hrlib/math/spline.cpp
+    ../hrlib/math/spline.cpp \
+    ../hrlib/units/length.cpp
---
+++ src/hrlib/units/length.cpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+
+ Copyright (c) 2013, Hans Robeers
+ All rights reserved.
+
+ BSD 2-Clause License
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+****************************************************************************/
+
+#include "length.h"
+#include "boost/units/base_units/cgs/centimeter.hpp"
+#include "boost/units/systems/cgs/length.hpp"
+
+#include <QString>
+
+using namespace hrlib::units;
+using namespace boost::units;
+
+Length::Length(boost::units::quantity<si::length, qreal> internalValue, LengthUnit::e displayUnit)
+//    : UnitBase(internalValue)
+{
+    setInternalValue(internalValue);
+    _displayUnit = displayUnit;
+}
+
+qreal Length::value()
+{
+    if (_displayUnit == LengthUnit::m)
+    {
+        return _internalValue.value();
+    }
+    else if (_displayUnit == LengthUnit::cm)
+    {
+        quantity<cgs::length, qreal> converted(_internalValue);
+        return converted.value();
+    }
+
+    return qreal(0)/qreal(0);
+}
+
+void Length::setValue(qreal value)
+{
+    if (_displayUnit == LengthUnit::m)
+    {
+        quantity<si::length, qreal> m(value * si::meter);
+        _internalValue = m;
+    }
+    else if (_displayUnit == LengthUnit::cm)
+    {
+        quantity<si::length, qreal> cm(value * cgs::centimeter);
+        _internalValue = cm;
+    }
+}
+
+QString Length::unitSymbol()
+{
+    switch (_displayUnit) {
+    case LengthUnit::m:
+        return "m";
+
+    case LengthUnit::cm:
+        return "cm";
+
+    default:
+        return "";
+    }
+}
+
+QString Length::unitName()
+{
+    return "";
+}
+
+Length::~Length()
+{
+}
