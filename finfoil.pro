2d02bdae0635eaf245f4823cf2558c737f60f2c0
hrobeers <hansrobeers@gmail.com>
2014-02-24 08:18:51 +0100

    Separate test and main project files

--- finfoil.pro
+++ finfoil.pro
@@ -1,5 +1,3 @@
 TEMPLATE = subdirs
 
-SUBDIRS += \
-    src/app \
-    src/unittest
+SUBDIRS += src/app
--- src/version_autogen.h
+++ src/version_autogen.h
@@ -5,7 +5,7 @@
 #ifndef VERSION_AUTOGEN_H
 #define VERSION_AUTOGEN_H
 
-#define BUILD_NUMBER 320
-#define COMMIT_HASH "154ecbabc7df52fd24be23a8a0d712fe8f8f1344"
+#define BUILD_NUMBER 321
+#define COMMIT_HASH "78a24cd5d30fae0a2d1aa6f8cfb758be81a2cdee"
 
 #endif // VERSION_AUTOGEN_H
